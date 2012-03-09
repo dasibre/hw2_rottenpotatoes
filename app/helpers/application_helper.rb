93d90f6f18ac212d0f82566404f274814da5ed7f
James <james.naadjie@gmail.com>
2012-03-08 23:19:20 -0500

    css class conditional addition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-	def hidden_div_if(condition, attributes = {}, &block)
-		if condition
-			attributes["class"] = "th.hilite"
-		end
-		content_tag("div", attributes, &block)
-	end
 end
